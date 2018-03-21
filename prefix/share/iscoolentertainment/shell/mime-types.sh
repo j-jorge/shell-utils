f35750e98a0bf7c7f6e610a6a5d817882a13ca38
Julien Jorge <julien.jorge@stuff-o-matic.com>
2018-03-21 16:34:42 +0100

    Add a function to get a mime type formatted for the current host.

---
+++ prefix/share/iscoolentertainment/shell/mime-types.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+[ -z "$ISCOOL_MIME_TYPES_INCLUDED" ] || return 0
+ISCOOL_MIME_TYPES_INCLUDED=1
+
+. "$(dirname "${BASH_SOURCE[0]}")/platform.sh"
+
+mime_type_name()
+{
+    case "$1" in
+        gzip)
+            is_osx && echo "application/x-gzip" || echo "application/gzip"
+            ;;
+        *)
+            printf "Unsupported type for mime types: %s.\n" "$1" >&2
+            ;;
+    esac
+}
+

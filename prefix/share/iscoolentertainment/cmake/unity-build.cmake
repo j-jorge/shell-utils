84d43542d51220fea9ad2a32f9d7af69f006297a
Julien Jorge <julien.jorge@stuff-o-matic.com>
2018-10-05 15:40:39 +0200

    Revert "Create as many compilation units as there are cpus."
    This reverts commit a2d711f2cbdab75c853c00c7590a6aed0fd99046.

--- prefix/share/iscoolentertainment/cmake/unity-build.cmake
+++ prefix/share/iscoolentertainment/cmake/unity-build.cmake
@@ -1,62 +1,32 @@
-include( ProcessorCount )
-
 function( make_compilation_unit unit_file name extension )
 
-  ProcessorCount( unit_count )
-  
-  if( ${unit_count} EQUAL 0 )
-    set( unit_count 1 )
-  endif()
+  set( temporary_unit "${PROJECT_BINARY_DIR}/${name}.unit.tmp" )
+  file( REMOVE "${temporary_unit}" )
 
-  list( LENGTH ARGN arg_count )
-  
-  if( ${arg_count} LESS ${unit_count} )
-    set( unit_count ${arg_count} )
-  endif()
-  
-  set( temporary_units )
-
-  foreach( unit_index RANGE 1 ${unit_count} )
-    set( temp "${PROJECT_BINARY_DIR}/${name}-unit.tmp.${unit_index}" )
-    file( REMOVE "${temp}" )
-    set( temporary_units ${temporary_units} ${temp} )
-  endforeach()
-
-  set( unit_index 0 )
-  
   foreach( f ${ARGN} )
-    list( GET temporary_units ${unit_index} temp )
-    file( APPEND "${temp}" "#include \"${f}\"\n" )
-
-    math( EXPR unit_index "${unit_index} + 1" )
-
-    if( ${unit_index} EQUAL ${unit_count} )
-      set( unit_index 0 )
-    endif()
+    file( APPEND "${temporary_unit}" "#include \"${f}\"\n" )
   endforeach()
 
-  set( result )
+  set( final_unit "${PROJECT_BINARY_DIR}/${name}.unit.${extension}" )
 
-  foreach( unit_index RANGE 1 ${unit_count} )
-    set(
-      unit
-      "${PROJECT_BINARY_DIR}/${name}-unit-${unit_index}.${extension}"
-      )
-    set( result ${result} ${unit} )
+  execute_process( COMMAND ${CMAKE_COMMAND}
+    -E compare_files
+    "${temporary_unit}"
+    "${final_unit}"
+    RESULT_VARIABLE need_update
+    OUTPUT_QUIET
+    ERROR_QUIET
+    )
 
-    math( EXPR list_index "${unit_index} - 1" )
-    list( GET temporary_units ${list_index} temp )
-
+  if( need_update )
     execute_process( COMMAND ${CMAKE_COMMAND}
-      -E copy_if_different
-      "${temp}"
-      "${unit}"
-      OUTPUT_QUIET
-      ERROR_QUIET
+      -E copy
+      "${temporary_unit}"
+      "${final_unit}"
       )
+  endif()
 
-    file( REMOVE "${temp}" )
-  endforeach()
+  file( REMOVE "${temporary_unit}" )
 
-  set( ${unit_file} ${result} PARENT_SCOPE )
+  set( ${unit_file} ${final_unit} PARENT_SCOPE )
 endfunction()

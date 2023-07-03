1a4cdb63b13f115264635ada9778c12e47838586
Julien Jorge <julien.jorge@stuff-o-matic.com>
2023-07-03 21:01:38 +0200

    Disable terminal colors when not using a terminal.

--- prefix/share/iscoolentertainment/shell/colors.sh
+++ prefix/share/iscoolentertainment/shell/colors.sh
@@ -3,18 +3,33 @@
 [ -z "${ISCOOL_COLORS_INCLUDED:-}" ] || return 0
 ISCOOL_COLORS_INCLUDED=1
 
-[ -t 1 ] || return 0
+if [[ -t 1 ]]
+then
+    blue="\033[0;34m"
+    blue_bold="\033[1;34m"
 
-blue="\033[0;34m"
-blue_bold="\033[1;34m"
+    green="\033[0;32m"
+    green_bold="\033[1;32m"
 
-green="\033[0;32m"
-green_bold="\033[1;32m"
+    red="\033[0;31m"
+    red_bold="\033[1;31m"
 
-red="\033[0;31m"
-red_bold="\033[1;31m"
+    yellow="\033[0;33m"
+    yellow_bold="\033[1;33m"
 
-yellow="\033[0;33m"
-yellow_bold="\033[1;33m"
+    term_color="\033[0;0m"
+else
+    blue=""
+    blue_bold=""
 
-term_color="\033[0;0m"
+    green=""
+    green_bold=""
+
+    red=""
+    red_bold=""
+
+    yellow=""
+    yellow_bold=""
+
+    term_color=""
+fi

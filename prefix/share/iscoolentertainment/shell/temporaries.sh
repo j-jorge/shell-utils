bfaa4403e50ae92829c6054a2e9146fee6d8c57f
Julien Jorge <julien.jorge@stuff-o-matic.com>
2024-08-13 11:09:59 +0100

    Replace hard-coded /tmp with TMPDIR.

--- prefix/share/iscoolentertainment/shell/temporaries.sh
+++ prefix/share/iscoolentertainment/shell/temporaries.sh
@@ -3,7 +3,7 @@
 [ -z "${ISCOOL_TEMPORARIES_INCLUDED:-}" ] || return 0
 ISCOOL_TEMPORARIES_INCLUDED=1
 
-export IC_TEMP_FILES=$(mktemp /tmp/tmp.XXXXXXXXXX)
+export IC_TEMP_FILES=$(mktemp "${TMPDIR:-/tmp}"/tmp.XXXXXXXXXX)
 
 trap 'cat $IC_TEMP_FILES | xargs rm -f -r $IC_TEMP_FILES' EXIT
 
@@ -12,7 +12,7 @@ make_temporary_file()
     local PREFIX="${1:-}"
     local RESULT
 
-    [ -n "$PREFIX" ] || PREFIX=/tmp/tmp.XXXXXXXXXX
+    [ -n "$PREFIX" ] || PREFIX="${TMPDIR:-/tmp}"/tmp.XXXXXXXXXX
 
     RESULT=$(mktemp "$PREFIX")
     echo "$RESULT" >> "$IC_TEMP_FILES"
@@ -25,7 +25,7 @@ make_temporary_directory()
     local PREFIX="${1:-}"
     local RESULT
 
-    [ -n "$PREFIX" ] || PREFIX=/tmp/tmp.XXXXXXXXXX
+    [ -n "$PREFIX" ] || PREFIX="${TMPDIR:-/tmp}"/tmp.XXXXXXXXXX
 
     RESULT=$(mktemp -d "$PREFIX")
     echo "$RESULT" >> "$IC_TEMP_FILES"
--- tests/temporaries.sh
+++ tests/temporaries.sh
@@ -13,4 +13,13 @@ TEMP_DIR=$(make_temporary_directory)
 [ -f "$TEMP_2" ] || test_failed $LINENO
 [ -d "$TEMP_DIR" ] || test_failed $LINENO
 
+custom_tmpdir="$TEMP_DIR"
+export TMPDIR="$custom_tmpdir"
+
+custom_1=$(make_temporary_file)
+custom_dir=$(make_temporary_directory)
+
+[ "$(dirname "$custom_1")" -ef "$custom_tmpdir" ] || test_failed $LINENO
+[ "$(dirname "$custom_dir")" -ef "$custom_tmpdir" ] || test_failed $LINENO
+
 test_end
